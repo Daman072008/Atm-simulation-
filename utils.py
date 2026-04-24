02eba1b8858853d4899ecf2a5c1f400637f8d553
Daman072008 <r89463985@gmail.com>
2026-04-24 14:31:24 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,6 @@
+username=["Arsh","Amit","Bipan","Daman","Anatariksh","Abhishek"]
+unique_user_ids=[2546077,2546058,2546097,2546104,2546070,2546025]
+unique_pins=[6077,6058,6097,6104,6070,6025]
+balances=[1000000,2000000,3000000,4000000,5000000,6000000]
+transactions=[[],[],[],[],[],[]]
+
